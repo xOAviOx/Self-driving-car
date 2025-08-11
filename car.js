f2ffb061d89b66043632e3af43193c3d21474c0c
Avi shukla <avishuklacode.com>
2025-08-11 18:20:13 +0530

    Car movements(Fwd + Bwd)

--- car.js
+++ car.js
@@ -4,8 +4,38 @@ class Car {
     this.y = y;
     this.width = width;
     this.height = height;
-
+    this.speed = 0;
+    this.acceleration = 0.2;
     this.controls = new Controls();
+    this.maxSpeed = 3;
+    this.friction = 0.05;
+  }
+
+  update() {
+    if (this.controls.forward) {
+      this.speed += this.acceleration;
+    }
+    if (this.controls.reverse) {
+      this.speed -= this.acceleration;
+    }
+
+    if (this.speed > this.maxSpeed) {
+      this.speed = this.maxSpeed;
+    }
+    if (this.speed < -this.maxSpeed / 2) {
+    }
+
+    if (this.speed > 0) {
+      this.speed -= this.friction;
+    }
+    if (this.speed < 0) {
+      this.speed += this.friction;
+    }
+    if (Math.abs(this.speed) < this.friction) {
+      this.speed = 0;
+    }
+
+    this.y -= this.speed;
   }
 
   draw(ctx) {

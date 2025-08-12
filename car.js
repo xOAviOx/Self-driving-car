bc9fd23c8afb3903cf04acdfd81f22d1f55ccb2c
Avi shukla <avishuklacode.com>
2025-08-12 13:58:53 +0530

    Car mechanics

--- car.js
+++ car.js
@@ -36,24 +36,28 @@ class Car {
       this.speed = 0;
     }
 
-    if (this.controls.left) {
-      this.angle += 0.03;
-    }
-    if (this.controls.right) {
-      this.angle -= 0.03;
+    if (this.speed != 0) {
+      const flip = this.speed > 0 ? 1 : -1;
+      if (this.controls.left) {
+        this.angle += 0.03 * flip;
+      }
+      if (this.controls.right) {
+        this.angle -= 0.03 * flip;
+      }
     }
 
-    this.y -= this.speed;
+    this.x -= Math.sin(this.angle) * this.speed;
+    this.y -= Math.cos(this.angle) * this.speed;
   }
 
   draw(ctx) {
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.rotate(-this.angle);
     ctx.beginPath();
-    ctx.rect(
-      this.x - this.width / 2,
-      this.y - this.height / 2,
-      this.width,
-      this.height
-    );
+    ctx.rect(-this.width / 2, -this.height / 2, this.width, this.height);
     ctx.fill();
+
+    ctx.restore();
   }
 }
